eceba296986c87818cc916e15a55e2b32d1035b6
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2022-09-09 09:18:46 -0300

    Day2Commit

---
+++ WebDevDay2/parent.js
@@ -0,0 +1,13 @@
+console.log("Hi there");
+
+console.log('Hello there');
+
+console.log(`Hey Stevie!`);
+
+let firstName = "Stevie";
+
+console.log(`Earlier first name was: `+ firstName);
+
+firstName="Pinky";
+
+console.log(`Now first name is: `+ firstName);
