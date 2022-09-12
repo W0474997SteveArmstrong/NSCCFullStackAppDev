71d17e733d7e5505829838a2e642dbb46d3afd3e
Armstrong <W0474997@campus.nscc.ca>
2022-09-12 10:07:43 -0300

    DataTypes

---
+++ Sept12/datatype.js
@@ -0,0 +1,24 @@
+// // let myName = "Steve";
+// // myName = "Stevie";
+
+// // console.log(myName);
+
+// const rls = require('readline-sync');
+
+// // const rls = require('readline-sync').
+// // question("Who is your favorite Singer?").
+// // question("What is your favorite food?");
+
+
+// const q1 = rls.question("Who is your favorite Singer?");
+// const q2 = rls.question("What is your favorite food?");
+
+// // console.log(rls);
+// console.log(q1);
+console.log(typeof(5));
+console.log(typeof("5"));
+console.log(typeof(3+"Steve"));
+console.log(typeof(3/"Steve"));
+
+
+
