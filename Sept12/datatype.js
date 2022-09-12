e97ec01e50f4ff7a97172b8c1b68131ec6555954
Armstrong <W0474997@campus.nscc.ca>
2022-09-12 11:06:36 -0300

    If statements

--- Sept12/datatype.js
+++ Sept12/datatype.js
@@ -5,20 +5,30 @@
 
 // const rls = require('readline-sync');
 
-// // const rls = require('readline-sync').
-// // question("Who is your favorite Singer?").
-// // question("What is your favorite food?");
+// const rls = require('readline-sync').
+// question("Who is your favorite Singer?").
+// question("What is your favorite food?");
 
 
 // const q1 = rls.question("Who is your favorite Singer?");
 // const q2 = rls.question("What is your favorite food?");
+// const rls = require('readline-sync');
+// const myOldName = rls.
+// question("What is your name?");
 
 // // console.log(rls);
 // console.log(q1);
-console.log(typeof(5));
-console.log(typeof("5"));
-console.log(typeof(3+"Steve"));
-console.log(typeof(3/"Steve"));
 
+// console.log(typeof(5));
+// console.log(typeof("5"));
+// console.log(typeof(3+"Steve"));
+// console.log(typeof(3/"Steve"));
 
+let myName="Steve"
+if (myName==myOldName){
+    console.log("Steve's hot");
+}
+else{
+    console.log("Steve is super hot");
+}
 
