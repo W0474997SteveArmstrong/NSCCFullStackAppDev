5be7d6789d52a647cc90643fb23da6bf41c41834
Armstrong <W0474997@campus.nscc.ca>
2022-09-14 09:47:39 -0300

    do while loop practice

--- Sept14/index.js
+++ Sept14/index.js
@@ -1,6 +1,13 @@
 const rls = require('readline-sync');
 let question1 = null;
-while(("Dean"!=question1) || (null==question1)){
+// while(("Dean"!=question1) || (null==question1)){
+//     question1 = rls.question("Hi there! Please key in your name: ");
+// }
+// console.log("Hello Dean");
+
+do{
     question1 = rls.question("Hi there! Please key in your name: ");
 }
+while("Dean"!=question1);
 console.log("Hello Dean");
+
