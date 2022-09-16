83e50b21460802fd82ced3bd4476c7bf004e28be
Armstrong <W0474997@campus.nscc.ca>
2022-09-16 10:20:18 -0300

    For loop practice

---
+++ Sept16ForLoopPractice/forloopPractice.js
@@ -0,0 +1,9 @@
+for (let i=0;i<=10;i++){
+    console.log("i: ",i);
+    for(let j=0;j<=5;j++){
+        if(j==2 || j==3){
+            break;
+        }
+        console.log('j: ',j);
+    }
+}
---
+++ Sept16ForLoopPractice/switchCase.js
@@ -0,0 +1,14 @@
+const nameValue = "Dean";
+//switch case break
+switch(nameValue){
+
+    case "Fred":
+        console.log("Hi Fred");
+    case "Dean":
+        console.log("Hey Dean");
+        break;   
+    case "Fluer":
+        console.log("Hi Fluer");
+    default:
+        console.log("goodbye!");
+}
