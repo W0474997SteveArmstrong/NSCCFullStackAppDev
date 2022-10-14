0d769ee2e87c24208b8364ccb0f9f8ff1ff7c0bb
Armstrong <W0474997@campus.nscc.ca>
2022-10-14 15:40:36 -0300

    Covered two test case solutions

--- Assignment2APPD5000/question2.js
+++ Assignment2APPD5000/question2.js
@@ -1,7 +1,7 @@
-const CHOICEARR=["a","b","c","d","e"];
+const CHOICEARR = ["a", "b", "c", "d", "e"];
 const RLS = require('readline-sync');
 //1.Display AnimalName Menu
-function animalNameMenu(){
+function displayAnimalNameMenu() {
     console.log("The Itsy Bitsy Aardvark");
     console.log("");
     console.log("Please choose an animal name:");
@@ -10,9 +10,9 @@ function animalNameMenu(){
     console.log("c) duck");
     console.log("d) pelican");
     console.log("e) t-rex");
-    }
-animalNameMenu();
-function actionWordMenu(){
+}
+displayAnimalNameMenu();
+function displayActionWordMenu() {
     console.log("Please choose an action word ending in 'ed': ");
     console.log("a) jumped");
     console.log("b) flipped");
@@ -21,44 +21,54 @@ function actionWordMenu(){
     console.log("e) squashed");
 }
 //2.Take user input
-let userInput=null;
-let usrInp = function getUserInput(){
+let userInput = null;
+let usrInp = function getUserInput() {
     userInput = RLS.question("Enter choice (a-e):");
     return userInput;
 }
 usrInp();
-function checkIfInputIsValidWithoutDisplayingMenu(){
-    if(CHOICEARR.includes(userInput)){
-        console.log("End of program");
+//3
+let flag = 0;
+function checkIfInputIsValidWithoutDisplayingActionMenu() {
+    if (CHOICEARR.includes(userInput)) {
+        //4 correct input
+        //End of program
     }
-    //invalid input
-    else{
+    //7.invalid input
+    else {
         //7.Ask for user input again
         usrInp();
         //3. check if input is correct
-        while(!(CHOICEARR.includes(userInput))){
-            usrInp();
+        while (flag = 0) {
+            if ((CHOICEARR.includes(userInput))) {
+                //4 correct input
+                // End of Program
+            }
+            else {
+
+            }
         }
     }
 }
-//3. See if the input is correct or not
-function checkIfInputIsValid(){
-    if(CHOICEARR.includes(userInput)){
-        console.log("UserInput: ",userInput);
-        //4 display action menu
-        actionWordMenu();
+//3. Check if the input is correct or not
+function checkIfInputIsValidDisplayingActionMenu() {
+    if (CHOICEARR.includes(userInput)) {
+        //4.correct input
+        console.log("UserInput: ", userInput);
+        //5 display action menu
+        displayActionWordMenu();
         //2 Take userInput for action menu
         usrInp();
         //3 or 6
-        checkIfInputIsValidWithoutDisplayingMenu();
+        checkIfInputIsValidWithoutDisplayingActionMenu();
         //5.Program ends here in this valid input scenario
     }
-    else{
+    else {
 
     }
 }
-
-
+//3.Check if the input is valid or not.
+checkIfInputIsValidDisplayingActionMenu();
 
 // //first time method call for animal name. User entered a valid input
 // //3.Check if the input is correct or not.
@@ -70,22 +80,21 @@ function checkIfInputIsValid(){
 //     usrInp();
 //     //3 checks if input is valid or not
 //     checkIfInputIsValid();
-    //5.Program ends here in this valid input scenario
+//5.Program ends here in this valid input scenario
 
-    //3.Check if the input is valid or not.
-    checkIfInputIsValid();
+
 
 //6 Else is entered for invalid input scenario
-if(""){
-    console.log("Incorrect input, please enter a valid input");
-    //7.for taking input for animal menu in the event user entered an invalid input
-        usrInp();
-        console.log("UserInput after do is: ",userInput);
-        console.log(CHOICEARR.includes(userInput));
-        //3.Check if input is valid
-   checkIfInputIsValid();
-    else{
-        console.log("Invalid input.Please enter a valid one");
-        usrInp();
-    }
-}
+// if ("") {
+//     console.log("Incorrect input, please enter a valid input");
+//     //7.for taking input for animal menu in the event user entered an invalid input
+//     usrInp();
+//     console.log("UserInput after do is: ", userInput);
+//     console.log(CHOICEARR.includes(userInput));
+//     //3.Check if input is valid
+//     checkIfInputIsValid();
+//     else {
+//         console.log("Invalid input.Please enter a valid one");
+//         usrInp();
+//     }
+// }
