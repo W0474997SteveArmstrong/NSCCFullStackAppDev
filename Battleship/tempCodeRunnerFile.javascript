cf7481de9a1a94c188a0690760eb90d5525fc65c
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2022-10-29 03:02:14 -0300

    Code Cleanup

--- Battleship/tempCodeRunnerFile.javascript
+++
@@ -1,8 +0,0 @@
-const arr1 =[1,2,3,4,5]
-arr1.push(6);
-arr1
-let arr2 = [];
-arr2.push(arr1)
-arr2
-
-
--- Battleship/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const numbers = [65, 44, 12, 4];
-// const newArr = numbers.map(myFunction);
-
-// document.getElementById("demo").innerHTML = newArr;
-
-// function myFunction(num) {
-//   return num * 10;
-// }
