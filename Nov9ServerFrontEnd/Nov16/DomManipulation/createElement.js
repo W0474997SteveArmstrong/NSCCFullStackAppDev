9f0c331be4fe69c7a1faa5fd759e8ef503e7e8a2
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2022-11-16 20:04:17 -0400

    Create element, innerText, add element to body

---
+++ Nov9ServerFrontEnd/Nov16/DomManipulation/createElement.js
@@ -0,0 +1,31 @@
+// const body = document.body;
+
+// //This creates a reference to a div element
+// const divElem = document.createElement('div');
+
+// //This adds the divElem to our html i.e append the element to the body.
+// body.append(divElem);
+
+// //Add text to our divElem
+
+// divElem.innerText='Hello World';
+
+
+//To write in one line:
+// document.body.append(document.createElement('div').innerText='HelloWorld');
+
+
+
+//Practice without seeing:
+
+// const body = document.body;
+// const divElem = document.createElement('div');
+// body.append(divElem.innerText='Hello World');
+
+
+
+
+//Practice without seeing 2:
+
+document.body.append(document.createElement('div').innerText='Hello Beautiful World');
+
