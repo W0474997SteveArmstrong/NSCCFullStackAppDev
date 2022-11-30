5f74645910dd856bb2c3c628f9df09248e8aa8cd
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2022-11-30 12:21:24 -0400

    Pop up changed to single names

--- UpdateUI/script.js
+++ UpdateUI/script.js
@@ -1,26 +1,25 @@
-const tdTags = document.querySelectorAll('td');
+const tdTags = document.querySelectorAll("td");
 
-for(const tdTag of tdTags){
-tdTag.addEventListener('dblclick',makeEditable);
+for (const tdTag of tdTags) {
+  tdTag.addEventListener("dblclick", makeEditable);
 }
 
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll("button");
 
-for(const btn of btns){
-    btn.addEventListener('click',doUpdate);
+for (const btn of btns) {
+  btn.addEventListener("click", doUpdate);
 }
-function doUpdate(e){
-alert(e.target.parentElement.parentElement.innerText);
+function doUpdate(e) {
+  const allIds = e.target.parentElement.parentElement.children;
+  alert(allIds[0].textContent);
+  alert(allIds[2].textContent);
 }
-function makeEditable(e){
-    // console.dir(e.target);
-    //'dblclick and its properties get displayed in the console'
-    // console.dir(e.target);
-    //td element and its properties get returned.
+function makeEditable(e) {
+  // console.dir(e.target);
+  //'dblclick and its properties get displayed in the console'
+  // console.dir(e.target);
+  //td element and its properties get returned.
 
-    console.log(e.target);
-    e.target.setAttribute('contenteditable',true);
-
+  console.log(e.target);
+  e.target.setAttribute("contenteditable", true);
 }
-
-
